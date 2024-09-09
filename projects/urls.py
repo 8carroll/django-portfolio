dc89dc6dc92f133cda0788f50e58faa6464a0bd1
Brandon Carroll <bcrrll@amazon.com>
2024-09-09 09:24:05 -0700

    Finished project

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from projects import views
 
+app_name = 'projects'
 urlpatterns = [
-    path("", views.project_lists),
+    path('',views.all_projects, name='all_projects'),
+    path('<int:pk>', views.project_detail, name='project_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from projects.models import Project
 from django.http import HttpResponse
 
 # Create your views here.
-def project_lists(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # query db to return all projects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html',
+                  {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/details.html',
+                  {'project': project})
